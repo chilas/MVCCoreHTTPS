0c3cbef7cb19a9ece7d936f5d94ff24868675c8e
Chilezie Reginald Unachukwu <iamchilas@gmail.com>
2016-09-25 10:33:11 +0100

    FIX: crtfile name to coremvc.pfx

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +13,7 @@ namespace MVCCoreHTTPS
         public string certfile { get; set; }
         public Startup (IHostingEnvironment env)
         {
-            certfile = Path.Combine(env.ContentRootPath, "MVCCoreHTTPS.pfx");
+            certfile = Path.Combine(env.ContentRootPath, "coremvc.pfx");
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
