c8a12af142bd3e0ec7f551a6923101183076da9f
Chilezie Reginald Unachukwu <iamchilas@gmail.com>
2016-09-27 22:10:37 +0100

    ADD: User Secrets dependency

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -11,18 +12,23 @@ namespace MVCCoreHTTPS
     public class Startup
     {
         public string certfile { get; set; }
-        public Startup (IHostingEnvironment env)
+        public Startup(IHostingEnvironment env)
         {
             certfile = Path.Combine(env.ContentRootPath, "coremvc.pfx");
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets();
+            Configuration = builder.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvcCore();
             services.Configure<KestrelServerOptions>(options =>
             {
-                options.UseHttps(certfile, "jollof");
+                options.UseHttps(certfile, Configuration["Cert"]);
                 options.UseConnectionLogging();
             });
         }
